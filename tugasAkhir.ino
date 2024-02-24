5418c21aab4e86990cfaefc24eeb5456bc5f7750
IrfanHos029 <houseirfan02@gmail.com>
2024-02-25 02:27:52 +0700

    perf:menambahkan library sensor ultrasonik

--- tugasAkhir.ino
+++ tugasAkhir.ino
@@ -1,19 +1,22 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 #include <DFRobot_HX711.h>
+#include <HCSR04.h>
+
+HCSR04 hc(5, new int[3]{6, 7, 8}, 3); //initialisation class HCSR04 (trig pin , echo pin, number of sensor)
 
 DFRobot_HX711 BERAT(A2, A3);
 
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-const int trigPin1 = 9;  // Pin trigger sensor ultrasonik 1 (panjang)
-const int echoPin1 = 8;//10; // Pin echo sensor ultrasonik 1 (panjang)
-
-const int trigPin2 = 7;//11; // Pin trigger sensor ultrasonik 2 (lebar)
-const int echoPin2 = 6;//12; // Pin echo sensor ultrasonik 2 (lebar)
-
-const int trigPin3 = 13; // Pin trigger sensor ultrasonik 3 (tinggi)
-const int echoPin3 = A0; // Pin echo sensor ultrasonik 3 (tinggi)
+//const int trigPin1 = 9;  // Pin trigger sensor ultrasonik 1 (panjang)
+//const int echoPin1 = 8;//10; // Pin echo sensor ultrasonik 1 (panjang)
+//
+//const int trigPin2 = 7;//11; // Pin trigger sensor ultrasonik 2 (lebar)
+//const int echoPin2 = 6;//12; // Pin echo sensor ultrasonik 2 (lebar)
+//
+//const int trigPin3 = 13; // Pin trigger sensor ultrasonik 3 (tinggi)
+//const int echoPin3 = A0; // Pin echo sensor ultrasonik 3 (tinggi)
 
 //const int loadCellPin = A1; // Pin load cell
 
@@ -26,20 +29,21 @@ const float maxWidth = 50.0; // Batas Lebar maksimal dalam cm
 const float maxHeight = 50.0; // Batas Tinggi maksimal dalam cm
 const float maxWeight = 5.0; // Batas berat maksimal dalam kg
 
-int hasilP,hasilL=0;
+int hasilP,hasilL,hasilT=0;
+int length,height,width,weight;
 
 void setup() {
   Serial.begin(9600); // Inisialisasi komunikasi serial
-  lcd.init(); 
+  lcd.begin(); 
   lcd.backlight();
   lcd.setCursor(0,0);
  
-  pinMode(trigPin1, OUTPUT);
-  pinMode(echoPin1, INPUT);
-  pinMode(trigPin2, OUTPUT);
-  pinMode(echoPin2, INPUT);
-  pinMode(trigPin3, OUTPUT);
-  pinMode(echoPin3, INPUT);
+//  pinMode(trigPin1, OUTPUT);
+//  pinMode(echoPin1, INPUT);
+//  pinMode(trigPin2, OUTPUT);
+//  pinMode(echoPin2, INPUT);
+//  pinMode(trigPin3, OUTPUT);
+//  pinMode(echoPin3, INPUT);
 }
 long berat;
 void loop() {
@@ -65,23 +69,26 @@ void loop() {
 
 }
 
-float getDistance(int trigPin, int echoPin) {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-
-  float duration = pulseIn(echoPin, HIGH);
-  float distance = duration * 0.034 / 2; // Rumus jarak dari waktu
-
-  return distance;
-}
+//float getDistance(int trigPin, int echoPin) {
+//  digitalWrite(trigPin, LOW);
+//  delayMicroseconds(2);
+//  digitalWrite(trigPin, HIGH);
+//  delayMicroseconds(10);
+//  digitalWrite(trigPin, LOW);
+//
+//  float duration = pulseIn(echoPin, HIGH);
+//  float distance = duration * 0.034 / 2; // Rumus jarak dari waktu
+//
+//  return distance;
+//}
 
 void kalkulasi(){
-  int panjang = getDistance(trigPin1, echoPin1);
-  int lebar   = getDistance(trigPin2, echoPin2);  
-  int tinggi  = getDistance(trigPin3, echoPin3);  
+  int panjang = hc.dist(0);
+  delay(60);
+  int lebar   = hc.dist(1); 
+  delay(60);
+  int tinggi  = hc.dist(2);
+  delay(60); 
   
   if(panjang <= referenceLength){
     hasilP = referenceLength - panjang;
@@ -93,8 +100,8 @@ void kalkulasi(){
   }
   else{ hasilL = 0; }
   
-  if(tinngi <= heightDifference){
-    hasilT = referenceWidth - lebar;
+  if(tinggi <= referenceHeight){
+    hasilT = referenceHeight - lebar;
   }
   else{ hasilT = 0; }
 
@@ -104,15 +111,15 @@ void kalkulasi(){
 }
 
 void showMonitor(){
-  Serial.print(" Panjang: ");
-  Serial.println(length);
+  Serial.print("Panjang: ");
+  Serial.print(length);
   Serial.println(" cm");
 
-  Serial.print(" Lebar: ");
-  Serial.println(width);
+  Serial.print("Lebar: ");
+  Serial.print(width);
   Serial.println(" cm");
 
-  Serial.print(" Tinggi: ");
+  Serial.print("Tinggi: ");
   Serial.print(height);
   Serial.println(" cm");
 
