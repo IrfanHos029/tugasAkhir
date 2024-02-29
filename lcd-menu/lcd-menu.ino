3e015243887c07302d3e9982cb46e3a1cc891cbb
IrfanHos029 <houseirfan02@gmail.com>
2024-02-29 23:35:30 +0700

    feat:menambah menu gulir

--- lcd-menu/lcd-menu.ino
+++ lcd-menu/lcd-menu.ino
@@ -3,6 +3,7 @@
 #include <Wire.h>
 //#include <LCD.h>
 #include <LiquidCrystal_I2C.h>
+#include <EEPROM.h>
 
 LiquidCrystal_I2C  lcd(0x27,20,4);
 
@@ -15,7 +16,7 @@ int buttonPin = 8;
 OneButton button0(buttonPin, true);
 
 ///String menu1[]={"3","setBeban","setJarak","setLed"};
-String menu1[]={"3","kalibrasi Beban","Kalibrasi Jarak","Back"};
+String menu1[]={"5","kalibrasi Beban","Kalibrasi Jarak","Set Timer LCD","Set Timer Sleep","Back"};
 String menuJarak[]={"4","Sensor 1:","Sensor 2:","Sensor 3:","Back"};
 int currentLength;
 int currentLayer =0;
@@ -26,13 +27,20 @@ int currentChange = 0;
 int cursorLayer = 0;
 int subLayer=0;
 float parWeight=900;
-int valueLength = 49;
-int valueWidth = 49;
-int valueHeight = 49;
+int valueLength = 0;
+int valueWidth = 0;
+int valueHeight = 0;
 int flagS=0;
 long newPosition=0;
-
-
+int stepLayer=1;
+int lastStep;
+/*
+ * address  val
+ * 0        weight
+ * 1        sensor 1
+ * 2        sensor 2
+ * 3        sensor 3
+ */
 void setup() {
   
 //  button0.attachDoubleClick(doubleClick);
@@ -42,7 +50,15 @@ Serial.begin(9600);
   lcd.setBacklight(HIGH);
 button0.attachClick(singleClick);
 button0.attachDoubleClick(doubleclick1);
-
+parWeight = EEPROM.read(0);
+valueLength = EEPROM.read(1);
+valueWidth = EEPROM.read(2);
+valueHeight = EEPROM.read(3);
+delay(100);
+Serial.println(String() + "parWeight  :" + parWeight);
+Serial.println(String() + "valueLength:" + valueLength);
+Serial.println(String() + "valueWidth :" + valueWidth);
+Serial.println(String() + "valueHeight:" + valueHeight);
 }
 
 void loop() {
@@ -76,7 +92,7 @@ void singleClick(){
 
     case 3 :
       clearMenu();
-      
+      stepLayer = 1;
       currentLayer = 0;
       
     break;
@@ -90,6 +106,8 @@ void singleClick(){
   currentLayer = 1;
   currentSelect = 1;
   cursorSelect();
+  EEPROM.write(0,parWeight);
+  //EEPROM.commit();
  }
 
   if(currentLayer != 1 && subLayer == 2 ){
@@ -98,18 +116,21 @@ void singleClick(){
     case 1 :
       flagS=!flagS;
       clearMenu();
+     // if(flagS){ EEPROM.write(1,valueLength);}
       //Serial.println(String() + "falg run 1:" + flagS);
     break;
 
     case 2 :
       flagS=!flagS;
       clearMenu();
+     //  if(flagS){ EEPROM.write(2,valueWidth);}
       //Serial.println(String() + "falg run 2:" + flagS);
     break;
 
     case 3 :
      flagS=!flagS;
      clearMenu();
+    // if(flagS){ EEPROM.write(3,valueWeight);}
      //Serial.println(String() + "falg run 3:" + flagS);
     break;
 
@@ -120,6 +141,9 @@ void singleClick(){
       currentSelect = 2;
       flagS = 0;
       cursorSelect();
+      EEPROM.write(1,valueLength);
+      EEPROM.write(2,valueWidth);
+      EEPROM.write(3,valueHeight);
     break;
 
     
@@ -156,10 +180,12 @@ void singleClick(){
 
 void doubleclick1(){
   Serial.println("button 2 klik run");
+if(currentLayer == 0){
   currentLayer = 1;
   currentSelect = 1;
   cursorSelect();
   clearMenu();
+}
   }
 
 void getRotary(){
@@ -169,6 +195,7 @@ void getRotary(){
      // if(currentLayer != lastLayer){ clearMenu();}
       lastLayer = currentLayer;     
       currentLength = menu1[0].toInt();
+      //Serial.println(String()+"lastLayer2:"+lastLayer);
     break;
   };
   switch(subLayer){
@@ -204,6 +231,8 @@ void getRotary(){
     currentSelect--;
     cursorSelect();
   }
+
+
   
 //  Serial.println(String() + "newPosition:" + newPosition);
   //Serial.println(String() + "currentSelect:" + currentSelect);
@@ -242,10 +271,43 @@ void getRotary(){
       valueHeight--;
      }  
   }
+
+  if(currentLayer==1){
+      Serial.println(String()+"currentSelect:"+currentSelect);
+      if(currentSelect == 5 && stepLayer == 1){
+        stepLayer=0; 
+        if(currentSelect != lastStep){clearMenu();  }
+        Serial.println(String()+"lastLayer:"+lastLayer);
+        
+        }
+       if(currentSelect == 4 && stepLayer == 0){
+        stepLayer=1; 
+        if(currentSelect != lastStep){clearMenu();  }
+        Serial.println(String()+"lastLayer:"+lastLayer);
+        
+        }
+        lastStep = currentSelect; 
+    }
+   
+  Serial.println(String()+"newPosition     :"+newPosition);
+  Serial.println(String()+"oldPosition     :"+oldPosition);
+  //Serial.println(String()+"currentSelect:"+currentSelect);
   oldPosition = newPosition;
-
+ 
 }
-
+//if(currentLayer==1){
+//      if(currentSelect < 5){
+//        //stepLayer=1; 
+//        lastStep = currentSelect; 
+//        //Serial.println(String()+"lastLayer:"+lastLayer);
+//        }
+//      else if(currentSelect > 4){
+//        //stepLayer=2; 
+//        lastStep = currentSelect; 
+//       // Serial.println(String()+"lastLayer:"+lastLayer);
+//       }
+//      
+// }
 // Serial.println(String() + "currentSelect:" + currentSelect);
 // Serial.println(String() + "cursorLayer:" + cursorLayer);
 }
@@ -254,41 +316,76 @@ void showLcd(){
   int dataSensor[]={valueLength,valueWidth,valueHeight,0};
   if(currentLayer==0){
     lcd.setCursor(0,0);
-    lcd.print("Panjang:50");
-    //lcd.print((hasilP >= 50)? 0 : hasilP);
+    lcd.print("Panjang:");
+    lcd.print((valueLength<10)?"0"+String(valueLength):valueLength);
     lcd.setCursor(13,0);
     lcd.print(" cm");
     
     lcd.setCursor(0,1);
-    lcd.print("Lebar  :50");
-    //lcd.print((hasilL >= 50)? 0 : hasilL);
+    lcd.print("Lebar  :");
+    lcd.print((valueWidth<10)?"0"+String(valueWidth):valueWidth);
     lcd.setCursor(13,1);
     lcd.print(" cm");
     
     lcd.setCursor(0,2);
-    lcd.print("Tinggi :50");
-    //lcd.print((hasilT >= 50)? 0 : hasilT);
+    lcd.print("Tinggi :");
+    lcd.print((valueHeight<10)?"0" + String(valueHeight):valueHeight);
     lcd.setCursor(13,2);
     lcd.print(" cm");
 
     lcd.setCursor(0,3);
-    lcd.print("Berat  :500");
+    lcd.print("Berat  :");
+    //lcd.setCursor(0,3);
+    lcd.print(parWeight);
     //lcd.print((weight >= 1000)? weight / 1000 :  weight);
-    lcd.setCursor(13,3);
+    lcd.setCursor(14,3);
     lcd.print("Gram");
     
    
   }
-   if(currentLayer==1){
+//   if(currentLayer==1 && stepLayer == 1){
+//   // clearSelect();
+//    lastStep = stepLayer;
+//    lcd.setCursor(18,cursorLayer );
+//    lcd.print("<-");
+//
+////    for(int i=0;i<currentLength;i++){
+////      lcd.setCursor(0,i);
+////      lcd.print(menu1[i+1]);
+////    }
+//      for(int i=0;i<4;i++){
+//      lcd.setCursor(0,i);
+//      lcd.print(menu1[i+1]);
+//      //Serial.println(String()+"menu1:"+menu1[i+1]);
+//    }
+//    //Serial.println(String()+"lastLayer:"+lastLayer);
+//  }
+   if(currentLayer==1 ){
+    //clearSelect();
+    //lastStep = stepLayer;
     lcd.setCursor(18,cursorLayer );
     lcd.print("<-");
 
-    for(int i=0;i<currentLength;i++){
+    if(currentSelect < 5){
+      for(int i=0;i<4;i++){
       lcd.setCursor(0,i);
       lcd.print(menu1[i+1]);
+      //Serial.println(String()+"menu1:"+menu1[i+1]);
     }
+    }
+
+    if(currentSelect > 4){
+      for(int i=0;i<currentLength-4;i++){
+      lcd.setCursor(0,i);
+      lcd.print(menu1[5+i]);
+      //Serial.println(String()+"menu1:"+menu1[4+i]);
+      }
+   // Serial.println(String()+"lastLayer2:"+lastLayer);
+  }
   }
 
+   
+   
    if(subLayer==1){
     lcd.setCursor(0,0);
     lcd.print("BERAT:");
@@ -305,11 +402,17 @@ void showLcd(){
      for(int i=0;i<currentLength;i++){
       lcd.setCursor(0,i);
       lcd.print(menuJarak[i+1]);  
-      lcd.setCursor(10,(i==3)?2:i);
-      lcd.print(dataSensor[(i==3)?2:i]); 
+//      lcd.setCursor(10,(i==3)?2:i);
+//      lcd.print(dataSensor[(i==3)?2:i]); 
       lcd.setCursor(13,(i==3)?2:i);
       lcd.print(" CM");
     }
+    lcd.setCursor(10,0);
+    lcd.print((valueLength<10)?"0"+String(valueLength):valueLength); 
+    lcd.setCursor(10,1);
+    lcd.print((valueWidth<10)?"0"+String(valueWidth):valueWidth); 
+    lcd.setCursor(10,2);
+    lcd.print((valueHeight<10)?"0" + String(valueHeight):valueHeight); 
     if(flagS){lcd.setCursor(12,cursorLayer); lcd.print("*");}
   }
 
@@ -332,6 +435,18 @@ void cursorSelect(){
             cursorLayer = 2;
             break;
           case 4:
+            cursorLayer = 3;
+            break;
+          case 5:
+            cursorLayer = 0;
+            break;
+          case 6:
+            cursorLayer = 1;
+            break;
+          case 7:
+            cursorLayer = 2;
+            break;
+          case 8:
             cursorLayer = 3;
             break;
         };
